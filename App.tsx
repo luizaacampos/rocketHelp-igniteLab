458b24e49ac42362fb4cf17b7583e55e3dc142d2
Luiza Campos <luiza.campos@kroton.com.br>
2022-07-19 18:24:48 -0300

    day 2 done

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { NativeBaseProvider, StatusBar } from 'native-base';
 import { useFonts,  Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
-import { SignIn } from './src/screens/SignIn';
+
+import { Routes } from './src/routes';
 import { Loading } from './src/components/Loading';
 
 import { THEME } from './src/styles/theme';
@@ -16,7 +17,7 @@ export default function App() {
         translucent
       />
 
-      {fontsLoaded ? <SignIn /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </NativeBaseProvider>
   );
 }
